ae26b57048b48bc4054d175be8746626f5c336e7
Hamid Tabrizi <tabrizihamid6367@gmail.com>
2024-08-21 14:37:39 +0330

    test commit

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	server := NewApiServer(":4000")
+
+	store, err := NewPostgressStore()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	server := NewApiServer(":4000", store)
 	server.Run()
 
 	fmt.Println("Yeah Buddy!")
